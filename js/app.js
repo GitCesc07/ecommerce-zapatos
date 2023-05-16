642cd37622d344dbf9f1c2890c705c8c67af786f
CescDevGit07 <cesar04071997@gmail.com>
2023-05-16 17:12:30 -0600

    Panel de carrito ya se puede mostrar y ocultar con hacer click en el boton carrito

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 // Declaración de variables
 let menuVisible = false;
+let carritoVisible = false;
+
+// Mostrando panel del carrito de compra
+function mostrarCarrito() {
+  if (carritoVisible) {
+    document.getElementById("carrito-ocultar").classList = "";
+    carritoVisible = false;
+  }
+  else {
+    document.getElementById("carrito-ocultar").classList = "ocultar-carrito";
+    carritoVisible = true;
+  }
+}
+
 
 // Mostrar u ocultar el menú
 function mostrarOcultarMenu() {
